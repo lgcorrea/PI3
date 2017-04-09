46b4fba1c4eee8dc9f513f541520f4c088b4bba0
jose.guilherme <jose.guilherme@NOTESP344>
2017-04-09 13:33:59 -0300

    login

--- PI3/Login.Designer.cs
+++ PI3/Login.Designer.cs
@@ -28,59 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tela_Login));
             this.bt1 = new System.Windows.Forms.Button();
-            this.bt2 = new System.Windows.Forms.Button();
             this.tb1 = new System.Windows.Forms.TextBox();
             this.b2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // bt1
             // 
             this.bt1.BackColor = System.Drawing.Color.DodgerBlue;
             this.bt1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bt1.FlatAppearance.BorderSize = 2;
+            this.bt1.FlatAppearance.BorderSize = 0;
             this.bt1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
             this.bt1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
             this.bt1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt1.ForeColor = System.Drawing.Color.White;
-            this.bt1.Location = new System.Drawing.Point(156, 211);
+            this.bt1.Location = new System.Drawing.Point(119, 165);
             this.bt1.Name = "bt1";
             this.bt1.Size = new System.Drawing.Size(75, 38);
             this.bt1.TabIndex = 0;
-            this.bt1.Text = "Logar";
+            this.bt1.Text = "Entrar";
             this.bt1.UseVisualStyleBackColor = false;
             this.bt1.Click += new System.EventHandler(this.bt1_Click);
             // 
-            // bt2
-            // 
-            this.bt2.BackColor = System.Drawing.Color.DodgerBlue;
-            this.bt2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.bt2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bt2.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.bt2.FlatAppearance.BorderSize = 2;
-            this.bt2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
-            this.bt2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
-            this.bt2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bt2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt2.ForeColor = System.Drawing.Color.White;
-            this.bt2.Location = new System.Drawing.Point(17, 211);
-            this.bt2.Name = "bt2";
-            this.bt2.Size = new System.Drawing.Size(75, 38);
-            this.bt2.TabIndex = 1;
-            this.bt2.Text = "Sair";
-            this.bt2.UseVisualStyleBackColor = false;
-            this.bt2.Click += new System.EventHandler(this.bt2_Click);
-            // 
             // tb1
             // 
-            this.tb1.Location = new System.Drawing.Point(91, 108);
+            this.tb1.BackColor = System.Drawing.SystemColors.Window;
+            this.tb1.Location = new System.Drawing.Point(101, 56);
             this.tb1.Name = "tb1";
             this.tb1.Size = new System.Drawing.Size(140, 20);
             this.tb1.TabIndex = 2;
@@ -88,7 +64,7 @@
             // 
             // b2
             // 
-            this.b2.Location = new System.Drawing.Point(91, 161);
+            this.b2.Location = new System.Drawing.Point(101, 115);
             this.b2.Name = "b2";
             this.b2.Size = new System.Drawing.Size(140, 20);
             this.b2.TabIndex = 3;
@@ -100,8 +76,8 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 103);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(22, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 25);
             this.label1.TabIndex = 4;
@@ -113,56 +89,30 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(12, 156);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(17, 110);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 25);
             this.label2.TabIndex = 5;
             this.label2.Text = "Senha:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(358, 26);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(210, 223);
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(8, 26);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(286, 50);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Seja bem-vindo";
-            // 
             // Tela_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(580, 272);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.pictureBox2);
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(293, 236);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.b2);
             this.Controls.Add(this.tb1);
-            this.Controls.Add(this.bt2);
             this.Controls.Add(this.bt1);
             this.Name = "Tela_Login";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,13 +121,10 @@
         #endregion
 
         private System.Windows.Forms.Button bt1;
-        private System.Windows.Forms.Button bt2;
         private System.Windows.Forms.TextBox tb1;
         private System.Windows.Forms.TextBox b2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label3;
     }
 }
 
